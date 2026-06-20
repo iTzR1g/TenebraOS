62118c0d0b3472c146909836754fd4d7fd2afb05
iTzR1g <aymane.alae.90@gmail.com>
2026-06-20 16:03:51 +0100

    Fix critical issues: GRUB theme paths, BIOS modules, PolKit action, binary hooks, audio backend, sudoers perms, SDDM config

--- config/hooks/normal/0010-grub-theme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Binary stage: inject GRUB theme into ISO
-mkdir -p /boot/grub/themes/tenebra
-cat > /etc/default/grub.d/tenebra-theme.cfg << 'EOF'
-GRUB_THEME=/boot/grub/themes/tenebra/theme.txt
-GRUB_TIMEOUT=5
-GRUB_TIMEOUT_STYLE=menu
-GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"
-EOF
